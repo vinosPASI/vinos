217daf5ce10247d9b92314e6b8492d847d634ab2
erikaVeramendi <erikaveramendi16@gmail.com>
2026-04-04 10:55:54 -0400

    feat(net): [VM-34] implement dio client with http2 adapter

--- frontend/vinosfront/lib/core/network/dio_client.dart
+++ frontend/vinosfront/lib/core/network/dio_client.dart
@@ -1,3 +1,48 @@
+import 'package:dio/dio.dart';
+import 'package:dio_http2_adapter/dio_http2_adapter.dart';
+import 'package:flutter/foundation.dart'; // 👈 IMPORTANTE (para kIsWeb)
+import '../config/app_config.dart';
+
 class DioClient {
-  // se implementara despues
+  late final Dio dio;
+
+  DioClient() {
+    dio = Dio(
+      BaseOptions(
+        baseUrl: AppConfig.baseUrl,
+        connectTimeout: const Duration(seconds: 10),
+        receiveTimeout: const Duration(seconds: 10),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      ),
+    );
+
+    // 🔥 SOLO usar HTTP/2 si NO es web
+    if (!kIsWeb) {
+      dio.httpClientAdapter = Http2Adapter(
+        ConnectionManager(
+          idleTimeout: const Duration(seconds: 10),
+        ),
+      );
+    }
+
+    // 🔹 Interceptors (logs)
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          print("REQUEST[${options.method}] => ${options.uri}");
+          return handler.next(options);
+        },
+        onResponse: (response, handler) {
+          print("RESPONSE[${response.statusCode}] => ${response.data}");
+          return handler.next(response);
+        },
+        onError: (DioException e, handler) {
+          print("ERROR[${e.response?.statusCode}] => ${e.message}");
+          return handler.next(e);
+        },
+      ),
+    );
+  }
 }
--- frontend/vinosfront/lib/main.dart
+++ frontend/vinosfront/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'features/example/example_screen.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -11,9 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text('App Base')),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: ExampleScreen(), // 👈 AQUÍ mostramos tu pantalla de prueba
     );
   }
 }
