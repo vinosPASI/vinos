5b9e81f93039c762042ea1cfc8fce75b6f8afc23
Stuko0 <alexander436065lol@gmail.com>
2026-04-23 21:58:32 -0400

    feat(app): [VM-93] implement authentication repositories and registration screen

--- frontend/vinosfront/lib/features/auth/domain/repositories/auth_repository.dart
+++ frontend/vinosfront/lib/features/auth/domain/repositories/auth_repository.dart
@@ -2,4 +2,5 @@ import 'package:vinosfront/features/auth/domain/entities/user_model.dart';
 
 abstract class AuthRepository {
   Future<UserResponse> login(String email, String password);
+  Future<UserResponse> register(String email, String password, String name);
 }
